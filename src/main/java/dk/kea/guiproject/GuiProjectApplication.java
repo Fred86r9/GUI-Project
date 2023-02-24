e117aa5d1c0df4ef9f8ed80eb0905a198b56e59b
Fred86r9 <118350827+Fred86r9@users.noreply.github.com>
2023-02-24 13:26:19 +0100

    Initial commit

---
+++ src/main/java/dk/kea/guiproject/GuiProjectApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.guiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiProjectApplication.class, args);
+    }
+
+}
